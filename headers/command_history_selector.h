6b9d1a3483a1e764596ba910877664aae96175fb
j253805 <253805@usn.no>
2024-03-06 21:04:00 +0100

    Satt opp prototype og definisjon
    Skal sjekke etter "pil opp" task frem til "\n" møtes, deretter skal den valgte linjen returneres og kjøres.
    
    Denne linjen må nok brukes i jsh.c og kan fikses nå funksjonen skal tas i bruk, fokuser kun på basic funksjonalitet

--- headers/command_history_selector.h
+++ headers/command_history_selector.h
@@ -1,5 +1,11 @@
 #ifndef COMMAND_HISTORY_SELECTOR_H
 #define COMMAND_HISTORY_SELECTOR_H
 
+char *commandSelector(char ***history);
+
+char *commandSelector(char ***history)
+{
+    return "pass";
+}
 
 #endif /* COMMAND_HISTORY_SELECTOR_H */
